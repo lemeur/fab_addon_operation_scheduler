f3ba28c810816f45d1302d154634fb3d86f862c4
Thibault Le Meur <lemeurt@prd-etl-php.centralesupelec.fr>
2019-12-05 23:11:53 +0100

    Fixed display when args_schema is empty

--- fab_addon_operation_scheduler/static/fab_addon_operation_scheduler/js/main.js
+++ fab_addon_operation_scheduler/static/fab_addon_operation_scheduler/js/main.js
@@ -7,19 +7,24 @@ function loadJsonEditorSlaves(elem) {
                 master_schema = list_operations_schema[master_val]
                 theEditor = listOfJsonEditors['operation_args']
                 theEditor.destroy();
-                console.log(JSON.stringify(master_schema))
-                listOfJsonEditors['operation_args'] = init_json_editor('operation_args',JSON.stringify(master_schema),"{}","{}")
+                // master_schema might be undefined if there is no selected value
+                if (master_schema !== undefined && Object.keys(master_schema).length != 0) {
+                    //console.log(JSON.stringify(master_schema))
+                    listOfJsonEditors['operation_args'] = init_json_editor('operation_args',JSON.stringify(master_schema),"{}","{}")
+                }
         }
         else {
         }
         $('#' + master_id).on("change", function(e) {
             if (e.val) {
-                console.log(list_operations_schema[e.val])
+                //console.log(list_operations_schema[e.val])
                 master_schema = list_operations_schema[e.val]
                 theEditor = listOfJsonEditors['operation_args']
                 theEditor.destroy();
-                console.log(JSON.stringify(master_schema))
-                listOfJsonEditors['operation_args'] = init_json_editor('operation_args',JSON.stringify(master_schema),"{}","{}")
+                if (master_schema !== undefined &&  Object.keys(master_schema).length != 0) {
+                    //console.log(JSON.stringify(master_schema))
+                    listOfJsonEditors['operation_args'] = init_json_editor('operation_args',JSON.stringify(master_schema),"{}","{}")
+                }
             }
         })
     });
