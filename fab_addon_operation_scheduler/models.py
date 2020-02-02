d7b157a22058ef4478618e5f11343bb00a9e5f5a
Thibault Le Meur <lemeurt@prd-etl-php.centralesupelec.fr>
2020-02-02 01:20:02 +0100

    Smallf fix in view

--- fab_addon_operation_scheduler/models.py
+++ fab_addon_operation_scheduler/models.py
@@ -13,8 +13,6 @@ log = logging.getLogger(__name__)
 
 class ScheduledOperation(Model):
     id = Column(Integer, primary_key=True)
-    #operation_name = Column(Integer, ForeignKey('schedulable_operation.oper_name'), nullable=True)
-    #operation = relationship("SchedulableOperation")
     operation = Column(String(200), nullable=True)
     operation_args = Column(String(200), nullable=True)
     scheduler_args = Column(String(200), nullable=True)
@@ -29,7 +27,7 @@ class ScheduledOperation(Model):
         except:
             pass
 
-        return "{} {}({})".format(self.operation_name, trigger, self.scheduler_args)
+        return "{} {}({})".format(self.operation, trigger, self.scheduler_args)
 
     def get_dict(self):
         return {'id':self.id, 'operation':self.operation, 'operation_args':self.operation_args, 'scheduler_args':self.scheduler_args}
@@ -44,7 +42,7 @@ class ScheduledOperation(Model):
         if 'end_date' in taskSchedulerArgs and taskSchedulerArgs['end_date'] == "":
             del taskSchedulerArgs['end_date']
 
-        operation_function = addon_scheduler.get_func(self.operation_name)
+        operation_function = addon_scheduler.get_func(self.operation)
 
         if operation_function:
             if is_job_activated:
@@ -57,17 +55,17 @@ class ScheduledOperation(Model):
                     addon_scheduler.add_job(jobid, operation_function, **taskSchedulerArgs, max_instances=6, kwargs=operation_args_dict)
                     self.status="Registered"
                 except Exception as e:
-                    log.error("Error activating '{}' with error '{}'".format(self.operation_name, e))
-                    self.status="Error '{}'".format(self.operation_name, e)
+                    log.error("Error activating '{}' with error '{}'".format(self.operation, e))
+                    self.status="Error '{}'".format(self.operation, e)
             else:
                 try:
                     addon_scheduler.add_job(jobid, operation_function, **taskSchedulerArgs, max_instances=6)
                     self.status="Registered"
                 except Exception as e:
-                    log.error("Error activating '{}' with error '{}'".format(self.operation_name, e))
-                    self.status="Error '{}'".format(self.operation_name, e)
+                    log.error("Error activating '{}' with error '{}'".format(self.operation, e))
+                    self.status="Error '{}'".format(self.operation, e)
         else:
-            log.debug("APScheduler can't find oper function for:"+self.operation_name)
+            log.debug("APScheduler can't find oper function for:"+self.operation)
 
         log.debug("APScheduler jobs after ADD:'{}'".format(";".join(addon_scheduler.get_job_ids())))
 
